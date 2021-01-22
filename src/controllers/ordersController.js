40a88ecd5cc7968d0189b8bbd0e9972dbc1a92f1
KlaussVP <klauss.pinto@aluno.cefet-rj.br>
2021-01-21 22:40:20 -0300

    Merging OrderController Changes

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -4,19 +4,6 @@ const Product = require("../models/Product");
 
 class OrderController {
     async createOrder(orderData) {
-<<<<<<< HEAD
-        const { userId, products } = orderData;
-        const order = await Order.create({userId});
-        await this._createOrderProduct(order.id,products);
-        
-        return await Order.findByPk(order.id,{include:Product, through: OrderProduct});
-    };
-
-    async _createOrderProduct(orderId,products) {
-        products.forEach(async (p) => {
-            const {productId, quantity} = p;
-            await OrderProduct.create({ productId, quantity, orderId });
-=======
         const { userId, cart, addressId } = orderData;
         const order = await Order.create({ userId, addressId });
         await this._createOrderProduct(order.id, cart);
@@ -28,9 +15,8 @@ class OrderController {
         cart.forEach(async (i) => {
             const { product, quantity } = i;
             await OrderProduct.create({ productId: product.id, quantity, orderId });
->>>>>>> main
         });
-    };
+    }
 
     getAll(limit = null, offset = null) {
         return Order.findAll({
@@ -43,20 +29,14 @@ class OrderController {
                 }
             }]
         });
-    };
+    }
 
     async getOrderById(id) {
         const order = await Order.findByPk(id, { include: Product, through: OrderProduct });
         if (!order) throw new NotFoundError('Order not found');
 
         return order;
-<<<<<<< HEAD
-    };
-=======
     }
-
-
->>>>>>> main
 
     async deleteOrder(id) {
         const order = await Order.findByPk(id);
@@ -65,7 +45,7 @@ class OrderController {
         await OrderProduct.destroy({ where: { orderId: order.id } });
 
         await order.destroy();
-    };
+    }
 };
 
 module.exports = new OrderController();
