08d510b91a301eee74f12938eee956e0a13aae6c
Lucas Murakami <lkenjim98@gmail.com>
2021-01-21 23:22:06 -0300

    migration correction

--- migrations/20210121222705-create-addresdId-column-in-orders-table.js
+++ migrations/20210121222705-create-addresdId-column-in-orders-table.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('orders', 'addressId', {
       type: Sequelize.INTEGER,
-      allowNull: false,
+      allowNull: true,
       references: {
         model: 'addresses',
         key: 'id'
